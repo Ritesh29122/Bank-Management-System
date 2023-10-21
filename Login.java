34da89368d84c71368538cf5d08fbcaab298dbfb
Ritesh <rrbachhav251@gmail.com>
2023-10-21 11:54:49 +0530

    First commit

---
+++ Login.java
@@ -0,0 +1,13 @@
+package Bank.management.system;
+import javax.swing.*;
+public class Login extends JFrame{
+    Login()
+    {
+        setSize(800,480);
+        setVisible(true);
+    }
+    public static void main(String args[])
+    {
+        new Login();
+    }
+}
